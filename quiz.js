29919ff2533654f5eb99a922bb0d0c3fe7c643f1
Curtisaurus <ccdrullinger@gmail.com>
2021-04-22 23:00:48 -0400

    increased quiz functionality with saved scoring, timing fixes, and updating user on answer correctness

--- quiz.js
+++ quiz.js
@@ -21,7 +21,7 @@ var questions = [
         answer: "quotes"
     },
     {
-        clue: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+        clue: "A very useful tool used during development and debugging for printing content to the debugger is:",
         options: ["Javascript", "terminal / bash", "for loops", "console log"],
         answer: "console log"
     },
@@ -31,15 +31,18 @@ var questions = [
 var startBtn = document.querySelector("#start");
 var quizArea = document.querySelector("#quiz-area");
 var timerEl = document.querySelector("#timer");
+var resultArea = document.querySelector("#result-area");
 // setting initial values for timer and question index
 var timeLeft = 75;
 var qIndex = 0;
+// declaring setInterval variable as global to clear anywhere
+var quizTimer;
 
 // clears page, starts timer, and renders questions on start button click
 startBtn.addEventListener("click", function() {
     quizArea.innerHTML = "";
-    startTimer();
     renderQuiz();
+    startTimer();
 })
 
 // countdown timer function
@@ -50,6 +53,7 @@ function startTimer() {
 
         if (timeLeft <= 0) {
             clearInterval(quizTimer);
+            endQuiz();
         }
     }, 1000)
 }
@@ -77,13 +81,95 @@ function renderQuiz() {
 
 // checks if answer is correct
 function check(event) {
+    // declares variables for user selection and answer for question
     var guess = event.target.textContent;
     var answer = questions[qIndex].answer;
+    // creates a div to display result outside of quizArea (quizArea cleared on question render)
+    var result = document.createElement("div")
 
+    // compares answer and user selection, changing content of result div to reflect
     if (guess != answer) {
         timeLeft = timeLeft - 10;
+        result.textContent = "Incorrect";
+    } else {
+        result.textContent = "Correct!"
     }
 
+    // advances index for questions array - loads next question
     qIndex++;
-    renderQuiz();
+
+    // runs quiz if there are questions left, else stops timer runs end-of-quiz function
+    if (qIndex < questions.length) {
+        renderQuiz();
+    } else {
+        endQuiz();
+        clearInterval(quizTimer);
+    }
+
+    // show whether question was right or wrong for 2 seconds
+    resultArea.appendChild(result);
+    setTimeout(function() {
+        result.remove();
+    }, 2000);
+}
+
+function endQuiz() {
+    quizArea.innerHTML = "";
+    timerEl.textContent = "";
+
+    // creates "game over" text
+    var endText = document.createElement("h1");
+    endText.textContent = "Game Over";
+    quizArea.appendChild(endText);
+
+    // creates and displays final score
+    var scoreText = document.createElement("p");
+    scoreText.textContent = "Your final score was: " + timeLeft;
+    quizArea.appendChild(scoreText);
+
+    // creates label for input textbox
+    var inputLabel = document.createElement("label");
+    inputLabel.textContent = "Enter your initials:";
+    quizArea.appendChild(inputLabel);
+
+    // creates input textbox for initials
+    var initialBox = document.createElement("input");
+    quizArea.appendChild(initialBox);
+
+    // button to submit initials
+    var initialSubmit = document.createElement("button")
+    initialSubmit.setAttribute("type", "submit");
+    initialSubmit.textContent = "Submit"
+    quizArea.appendChild(initialSubmit)
+
+    // on clicking the submit button...
+    initialSubmit.addEventListener("click", function() {
+        // ...input box value is stored in variable...
+        var initials = initialBox.value;
+
+        // ...initials and score are assigned to an object...
+        var scoreboard = {
+            initials: initials,
+            score: timeLeft
+        }
+
+        // ...and high scores are retrieved from local
+        var highScores = localStorage.getItem("highScores");
+
+        // if high score array doesn't exist, start empty score array...
+        if (!highScores) {
+            highScores = [];
+        // ...otherwise parse string to array
+        } else {
+            highScores = JSON.parse(highScores);
+        }
+
+        // adds new initial + score object to score arry
+        highScores.push(scoreboard);
+        // stores locally
+        var scoresString = JSON.stringify(highScores);
+        localStorage.setItem("highScores", scoresString);
+        // load high scores page
+        window.location.replace("./highscores.html");
+    })
 }
